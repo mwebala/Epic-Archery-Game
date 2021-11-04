3d50f0101cc2f1e3bbe2819b2308d16b9d6e3eae
mwebala <87056880+mwebala@users.noreply.github.com>
2021-11-04 17:39:33 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,16 +20,16 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   angleMode(DEGREES);
-
+  var options ={
+    isStatic: true
+  }
   playerBase =Bodies.rectangle(200,350,180,150, options);
   World.add(world,playerBase);
 
   player = Bodies.rectangle(250, playerBase.position.y-160, 50, 180, options);
   World.add(world,player)
 
-  var options ={
-    isStatic: true
-  }
+
 
 
 
